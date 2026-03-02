777594f3f892c71e4cc6946322086f88d9118642
YaelDWeiss <yaelweiss25@gmail.com>
2026-03-02 10:52:32 +0200

    headless true

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def run():
             raise RuntimeError("Missing USERNAME/PASSWORD env vars")
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
